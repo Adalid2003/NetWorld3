2a4f06ecac360f3b27a6586b7d977d2b68bde119
WilliamOAC <williamamaya00729@gmail.com>
2021-05-09 21:32:31 -0600

    conect

--- app/helpers/database.php
+++ app/helpers/database.php
@@ -7,9 +7,9 @@ class Database{
 
     private static function connect(){
         $server = 'localhost';
-        $database = 'NetWorld31';
+        $database = 'NetWorld3';
         $username = 'postgres';
-        $password = 'rojo09';
+        $password = '20160062itr';
 
         self::$connection = new PDO('pgsql:host='.$server.';dbname='.$database.';port=5432', $username, $password);
     }

5bdb3e9a64a8d28213c8e528a85811d606854b2a
Adalid2003 <68620037+Adalid2003@users.noreply.github.com>
2021-05-12 22:43:02 -0600

    cambio estetico

--- app/helpers/dashboard_template.php
+++ app/helpers/dashboard_template.php
@@ -20,7 +20,7 @@ class Dashboard_page
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         
-        <link rel="shotcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="shotcut icon" href="../../resources/img/logo.jpg" type="image/x-icon">
     </head>
     
     <body>');

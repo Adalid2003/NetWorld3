a5dab93b12c457ca1244d2b8ff3203b00d9a3352
WilliamOAC <williamamaya00729@gmail.com>
2021-09-13 10:22:25 -0600

    solucion de error login

--- app/controllers/public/account.js
+++ app/controllers/public/account.js
@@ -18,7 +18,7 @@ document.addEventListener('click', function () {
                 if (response.status) {
                     sweetAlert(4, response.message, 'login.php');
                 } else {
-                    console.log('Sesión activa.');
+                    console.log('Sesión activa');
                 }
             });
         } else {
